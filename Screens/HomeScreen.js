1e0b656a486adab6216c422a8774fe4ba7b56ecd
Victor Harbo Johnston <vholesen@hotmail.com>
2024-01-10 20:52:49 +0100

    Add test api

--- Screens/HomeScreen.js
+++ Screens/HomeScreen.js
@@ -1,13 +1,31 @@
 import React from 'react';
 import { View, Text, StyleSheet } from 'react-native';
 import { TextInput } from 'react-native-paper';
+import { Button } from 'react-native-paper';
 
 
 function HomeScreen() {
     const [seachText, setText] = React.useState("");
+    const [data, setData] = React.useState(null);
 
     const handleSearchTextChange = (searchText) => {
       setText(searchText);
+    };
+
+    const fetchData = async () => {
+      try {
+        // Make a GET request to a sample JSONPlaceholder API endpoint
+        const response = await fetch('https://jsonplaceholder.typicode.com/todos/2');
+  
+        // Parse the response as JSON
+        const result = await response.json();
+  
+        // Update the state with the fetched data
+        setData(result);
+        console.log(result);
+      } catch (error) {
+        console.error('Error fetching data:', error);
+      }
     };
 
     return (
@@ -15,6 +33,14 @@ function HomeScreen() {
         <SearchField onChangeText={handleSearchTextChange} />
         <Text>Home!</Text>
         <Text>Currently reading: {seachText}</Text>
+        <TestButton fetchData={fetchData} />
+        {data && (
+          <View>
+            <Text>Data from API:</Text>
+            <Text>Title: {data.title}</Text>
+            <Text>Completed: {data.completed ? 'Yes' : 'No'}</Text>
+          </View>
+        )}
       </View>
     );
   }
@@ -30,6 +56,13 @@ const SearchField = ({ onChangeText }) => {
     />
   );
 };  
+
+const TestButton = ({fetchData}) => (
+  <Button mode="contained" 
+          onPress={fetchData}>
+    Test API Call
+  </Button>
+);
 
 const styles = StyleSheet.create({
   container: {
